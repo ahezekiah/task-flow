729ff559991c93ddb7b402b595b5dea8663d1a8f
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-01-15 18:38:20 -0700

    Added a few more random tasks

--- task-flow/src/App.jsx
+++ task-flow/src/App.jsx
@@ -6,12 +6,28 @@ function App() {
   const tasks =[
     {
       id: 1,
-      title: "title",
-      description: "random",
-      priority: "low",
+      title: "dishes",
+      description: "It is time to wash the dishes",
+      priority: "medium",
       dueDate: "2026-1-15",
       isCompleted: false 
     },
+    {
+      id: 2,
+      title: "clothes",
+      description: "It is time to put away the laundry",
+      priority: "medium",
+      dueDate: "2026-1-17",
+      isCompleted: false 
+    },
+    {
+      id: 3,
+      title: "food",
+      description: "Time to make a yummy meal",
+      priority: "low",
+      dueDate: "2026-1-15",
+      isCompleted: true 
+    }
   ]
 
   return (
