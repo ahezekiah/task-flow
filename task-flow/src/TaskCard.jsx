5c18a358564adb9505c3182d5639b0a14fbe2e19
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-01-15 18:32:02 -0700

    Added more classes to make styling easier

--- task-flow/src/TaskCard.jsx
+++ task-flow/src/TaskCard.jsx
@@ -1,12 +1,16 @@
 
 function TaskCard({task}){
     return(
-        <div>
-            <h1>Title: {task.title}</h1>
-            <p>Description: {task.description}</p>
-            <p>Priority: {task.priority}</p>
-            <p>Due Date: {task.dueDate}</p>
-            <p>Completed: {task.isCompleted ? "Yes" : "No"}</p>
+        <div class="container">
+            <div class="card">
+                <div class="tasks">
+                    <h1>Title: {task.title}</h1>
+                    <p>Description: {task.description}</p>
+                    <p>Priority: {task.priority}</p>
+                    <p>Due Date: {task.dueDate}</p>
+                    <p>Completed: {task.isCompleted ? "Yes" : "No"}</p>
+                </div>
+            </div>
         </div>
     )
 }
